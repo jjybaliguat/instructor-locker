7119d2b8aa14821df3fb900892410c98d15e4e0f
jjybaliguat <justinejeraldbaliguat@gmail.com>
2024-10-12 16:14:48 +0800

    some changes

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -24,7 +24,7 @@ function Home() {
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
+          <CardTitle className="text-md font-medium">
             Total Buses
           </CardTitle>
           <BusIcon />
