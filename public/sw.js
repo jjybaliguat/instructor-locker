103991fec7ae0228cdf926f61f1f0b39763a0061
jjybaliguat <justinejeraldbaliguat@gmail.com>
2025-04-22 16:27:27 +0800

    changed notification click event

--- public/sw.js
+++ public/sw.js
@@ -5,3 +5,20 @@ self.addEventListener("push", function (e) {
       icon: "/bus2.png",
     });
   });
+
+  self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+  
+    event.waitUntil(
+      clients.matchAll({ type: 'window' }).then(function (clientList) {
+        for (const client of clientList) {
+          if (client.url === event.notification.data.url && 'focus' in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(event.notification.data.url);
+        }
+      })
+    );
+  });
