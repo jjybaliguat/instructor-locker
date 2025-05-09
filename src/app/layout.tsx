16ddffe648391e6fbf65038da8228cd1fea10322
jjybaliguat <justinejeraldbaliguat@gmail.com>
2025-05-10 05:37:27 +0800

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Sidebar from "@/components/layout/sidebar";
-import Header from "@/components/layout/header";
-import MyProviders from "@/components/MyProviders";
-import NextTopLoader from 'nextjs-toploader';
-import Footer from "@/components/homepage-layout/Footer";
-import ScrollNav from "@/components/homepage-layout/ScrollNav";
-import { NavBar } from "@/components/homepage-layout/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
